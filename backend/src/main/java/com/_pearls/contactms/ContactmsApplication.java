541b835b7fc1edce856f26def022684eb1bba9c8
SafwanIrfan <mohdsafwan2k5@gmail.com>
2026-04-12 12:47:15 +0500

    Add Spring Boot backend

---
+++ backend/src/main/java/com/_pearls/contactms/ContactmsApplication.java
@@ -0,0 +1,13 @@
+package com._pearls.contactms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactmsApplication.class, args);
+	}
+
+}
