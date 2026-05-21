bc9ffbd439dd11d54870ed42f86d223258d1ab03
SafwanIrfan <mohdsafwan2k5@gmail.com>
2026-05-21 20:41:40 +0500

    testing sonarqube

---
+++ backend/src/test/java/com/_pearls/contactms/ContactmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com._pearls.contactms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
