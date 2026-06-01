c41554962ee146dac2a0172dd4dfbb4d68e596ef
SafwanIrfan <mohdsafwan2k5@gmail.com>
2026-06-01 22:08:45 +0500

    feat: refactor ActionButton component and update ProfilePage to use it for actions

--- frontend/src/features/user/components/ActionButton.tsx
+++ frontend/src/features/user/components/ActionButton.tsx
@@ -1,21 +1,32 @@
-// import type { LucideIcon } from "lucide-react";
+import { type LucideIcon } from "lucide-react";
 
-// interface ActionButtonProps {
-//     title: string,
-//     onClick: () => void,
-//     icon: LucideIcon
-// }
+interface ActionButtonProps {
+  title: string;
+  onClick: () => void;
+  icon: LucideIcon;
+  bgColor?: string;
+  variant?: "danger" | "primary";
+}
 
-// const ActionButton = () => {
-//     return (
-//       <button
-//         onClick={() => setShowChangePassword(true)}
-//         className="w-full flex items-center gap-3 px-5 py-4 text-sm font-medium text-text hover:bg-gray-50 transition-all rounded-t-2xl"
-//       >
+const ActionButton = ({
+  title,
+  onClick,
+  icon: Icon,
+  variant = "danger",
+}: ActionButtonProps) => {
+  const confirmStyles = {
+    danger: "bg-red-300 hover:bg-red-200 text-text",
+    primary: "hover:bg-gray-50 text-text",
+  };
+  return (
+    <button
+      onClick={onClick}
+      className={`w-full flex items-center gap-3 px-5 py-4 text-sm font-medium cursor-pointer transition-all rounded-2xl shadow-sm ${confirmStyles[variant]}`}
+    >
+      {Icon && <Icon size={15} strokeWidth={1.8} />}
+      {title}
+    </button>
+  );
+};
 
-//         Change Password
-//       </button>
-//     );
-// }
-
-// export default ActionButton
+export default ActionButton;
